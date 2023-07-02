0200e5aa880f8a4631be0f73b148d7679df1e0b6
Shah Poojan Dikeshkumar <shahpoojandikeshkumar@poojan.shah-C02D30Q6MD6W>
2023-07-02 22:43:40 +0530

    updated rep12

--- build-component.sh
+++ build-component.sh
@@ -6,8 +6,8 @@ TAG_TEMP=$2
 TAG=${TAG_TEMP:=1.0}
 TEMP=$3
 PUSH=${TEMP:=-NO}
-IMAGE=docker-bakery-system/${COMPONENT_NAME}:${TAG}
-
+# IMAGE=docker-bakery-system/${COMPONENT_NAME}:${TAG}
+IMAGE=poojan23/docker-bakery-system_${COMPONENT_NAME}:${TAG}
 
 function ping(){
     echo "Building from docker file path::"
@@ -15,16 +15,16 @@ function ping(){
 function check_component() {
    echo "COMPONENT_NAME: " ${COMPONENT_NAME}
   case ${COMPONENT_NAME} in
-    test-component1_repo12)
+    repo12-component1)
     DOCKERFILE_PATH=./components_repo12/test-component1_repo12/Dockerfile
     ;;
-    test-component2_repo12)
+    repo12-component2)
     DOCKERFILE_PATH=./components_repo12/test-component2_repo12/Dockerfile
     ;;
-    test-component3_repo12)
+    repo12-component3)
     DOCKERFILE_PATH=./components_repo12/test-component3_repo12/Dockerfile
     ;;
-    test-component4_repo12)
+    repo12-component4)
     DOCKERFILE_PATH=./components_repo12/test-component4_repo12/Dockerfile
     ;;
     *)
@@ -44,5 +44,52 @@ function build()
 
 check_component
 build
+
+
+
+
+# #!/usr/bin/env bash
+
+# #globals
+# COMPONENT_NAME=$1
+# TAG_TEMP=$2
+# TAG=${TAG_TEMP:=1.0}
+# TEMP=$3
+# PUSH=${TEMP:=-NO}
+# # IMAGE=docker-bakery-system/${COMPONENT_NAME}:${TAG}
+# IMAGE=poojan23/docker-bakery-system_${COMPONENT_NAME}:${TAG}
+
+# function ping(){
+#     echo "Building from docker file path::"
+# }
+# function check_component() {
+#    echo "COMPONENT_NAME: " ${COMPONENT_NAME}
+#   case ${COMPONENT_NAME} in
+#     repo3-component1)
+#     DOCKERFILE_PATH=./components_repo3/test-component1_repo3/Dockerfile
+#     ;;
+#     repo3-component2)
+#     DOCKERFILE_PATH=./components_repo3/test-component2_repo3/Dockerfile
+#     ;;
+#     repo3-component3)
+#     DOCKERFILE_PATH=./components_repo3/test-component3_repo3/Dockerfile
+#     ;;
+#     *)
+#       echo "Invalid component" && exit 1 ;;
+#   esac
+#   echo "<Building Image>
+#         dockerfile path ::" ${DOCKERFILE_PATH}"
+#         tag             ::" ${TAG}
+# }
+
+
+# function build()
+# {
+#     docker build -t ${IMAGE} -f "${DOCKERFILE_PATH}" .
+# }
+
+
+# check_component
+# build
 
 
